32781ecbe19ed702923386d801a7d4b2ce329371
sokolovalova <anisok170800@gmail.com>
2020-10-03 18:28:19 +0300

    Updates

---
+++ precache-manifest.d0fb463ae072c2c9dd30a9bbc8415119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebc3848d2416a3e895695a7667fa237",
+    "url": "/millionaire-quiz/index.html"
+  },
+  {
+    "revision": "954bb129f7504ee7e7bd",
+    "url": "/millionaire-quiz/static/css/main.11070f8a.chunk.css"
+  },
+  {
+    "revision": "d756a1c6a038f5110410",
+    "url": "/millionaire-quiz/static/js/2.ab1d25c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/millionaire-quiz/static/js/2.ab1d25c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954bb129f7504ee7e7bd",
+    "url": "/millionaire-quiz/static/js/main.9b6b232a.chunk.js"
+  },
+  {
+    "revision": "04821900b18f30519181",
+    "url": "/millionaire-quiz/static/js/runtime-main.620430e2.js"
+  }
+]);
